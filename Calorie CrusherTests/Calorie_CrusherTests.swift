79e9970d38fed76517f1ed0db2e8bb6bdfdf170c
Jeremiah Hicks <hicksjeremiahj@gmail.com>
2025-01-27 15:58:40 -0500

    Initial Commit

---
+++ Calorie CrusherTests/Calorie_CrusherTests.swift
@@ -0,0 +1,17 @@
+//
+//  Calorie_CrusherTests.swift
+//  Calorie CrusherTests
+//
+//  Created by Jeremiah Hicks on 1/27/25.
+//
+
+import Testing
+@testable import Calorie_Crusher
+
+struct Calorie_CrusherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
