cb965bf01fbff858bd372c5439ed6e5896fdcd9a
Davaughn Williams <313rdWay@gmail.com>
2025-02-05 13:28:14 -0500

    GameScreen & UI View

--- Calorie Crusher/GameScene.swift
+++ Calorie Crusher/GameScene.swift
@@ -10,79 +10,57 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func didMove(to view: SKView) {
+    let gameArea: CGRect
+    override init(size: CGSize) {
+        let maxAspectRatio: CGFloat = 16.0/7.2
+        let playableWidth = size.height / maxAspectRatio
+        let margin = (size.width - playableWidth) / 2
+        gameArea = CGRect(x: margin, y: 0, width: playableWidth, height: size.height)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        super.init(size: size)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    override func didMove(to: SKView) {
+        // Background
+        let background = SKSpriteNode(imageNamed: "staticBackground")
+        background.size = self.size
+        background.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
+        background.zPosition = 0
+        self.addChild(background)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        let player = SKSpriteNode(imageNamed: "happyCharacter")
+        player.setScale(2)
+        player.position = CGPoint(x: self.size.width/2, y: player.size.height * 1.78)
+        player.zPosition = 2
+        self.addChild(player)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        let scoreLabel = SKLabelNode(fontNamed: "SF Pro Rounded")
+        scoreLabel.text = "Score"
+        scoreLabel.fontSize = 80
+        scoreLabel.fontColor = .black
+        scoreLabel.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.left
+        scoreLabel.position = CGPoint(x: self.size.width * 0.22, y: self.size.height + scoreLabel.frame.size.height)
+        scoreLabel.zPosition = 100
+        self.addChild(scoreLabel)
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        let scoreLabelBackground = SKSpriteNode
+        
+        let caloriesLabel = SKLabelNode(fontNamed: "SF Pro Rounded")
+        caloriesLabel.text = "Calories"
+        caloriesLabel.fontSize = 50
+        caloriesLabel.fontColor = .white
+        caloriesLabel.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.right
+        caloriesLabel.position = CGPoint(x: self.size.width * 0.78, y: self.size.height + caloriesLabel.frame.size.height)
+        caloriesLabel.zPosition = 100
+        self.addChild(caloriesLabel)
+        
+        let moveOnToScreenAction = SKAction.moveTo(y: self.size.height * 0.9, duration: 0.3)
+        scoreLabel.run(moveOnToScreenAction)
+        caloriesLabel.run(moveOnToScreenAction)
     }
 }
--- Calorie Crusher/GameViewController.swift
+++ Calorie Crusher/GameViewController.swift
@@ -8,28 +8,30 @@
 import UIKit
 import SpriteKit
 import GameplayKit
+import AVFoundation
 
 class GameViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
-    }
+    override func viewDidLoad() {
+           super.viewDidLoad()
+        
+        
+           if let view = self.view as! SKView? {
+               // Load the SKScene from 'GameScene.sks'
+               let scene = GameScene(size: CGSize(width: 1536, height: 2048))
+               // Set the scale mode to scale to fit the window
+               scene.scaleMode = .aspectFill
+                   
+               // Present the scene
+               view.presentScene(scene)
+               
+               view.ignoresSiblingOrder = true
+               
+               view.showsFPS = false
+               view.showsNodeCount = false
+           }
+
+       }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
